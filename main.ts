2509174a513a02a45c5533e4ce1436cdbf3e1f5a
romain <100491683+Romain-2@users.noreply.github.com>
2024-02-07 09:53:04 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,8 @@
 let notre_humidite = 0
 basic.showString("HUMIDITE")
 basic.forever(function () {
-    notre_humidite = gatorSoil.moisture(AnalogPin.P0, gatorSoilType.soilMoisture, DigitalPin.P1)
+    notre_humidite = gatorSoil.moisture(AnalogPin.P0, gatorSoilType.adcVal, DigitalPin.P1)
     serial.writeValue("x", notre_humidite)
-    basic.pause(500)
     if (notre_humidite < 300) {
         basic.showIcon(IconNames.Sad)
     } else if (notre_humidite > 600) {
